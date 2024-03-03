b751200729f5e2947a927488f51997227921051d
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-03 21:08:05 +0300

    user ve admin icin test amacli getmapping yapildi

---
+++ src/main/java/com/workintech/ecommerce/backend/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.workintech.ecommerce.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String sayHi(){
+        return "This is admin area";
+    }
+}
