c9794145272eaddb62e87e6d4c0dc3fa32bb5bc9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-05 13:10:24 +0300

    global exception handler olusturuldu

---
+++ src/main/java/com/workintech/ecommerce/backend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.workintech.ecommerce.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionResponse> handleException(ProductException productException) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(productException.getMessage(), productException.getHttpStatus().value(),
+                LocalDateTime.now());
+        return new ResponseEntity<>(exceptionResponse, productException.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionResponse> handleException(CategoryException categoryException) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(categoryException.getMessage(), categoryException.getHttpStatus().value(),
+                LocalDateTime.now());
+        return new ResponseEntity<>(exceptionResponse, categoryException.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
