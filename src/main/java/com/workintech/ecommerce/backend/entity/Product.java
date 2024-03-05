c604a44c0193a9f741291249a9665f177caaccc9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-05 13:03:43 +0300

    product entitysi olusturuldu

---
+++ src/main/java/com/workintech/ecommerce/backend/entity/Product.java
@@ -0,0 +1,49 @@
+package com.workintech.ecommerce.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product", schema = "ecommerce")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "stock")
+    private Integer stock;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "sell_count")
+    private Integer sellCount;
+
+    @Column(name = "images")
+    private String[] images;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,
+            CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
