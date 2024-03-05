d579016e39a670b936b87e6ea82a7470a333c287
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-05 13:07:36 +0300

    crossorigin anotasyonu eklendi

--- src/main/java/com/workintech/ecommerce/backend/controller/AuthController.java
+++ src/main/java/com/workintech/ecommerce/backend/controller/AuthController.java
@@ -4,13 +4,11 @@ import com.workintech.ecommerce.backend.dto.RegisterUserDto;
 import com.workintech.ecommerce.backend.entity.User;
 import com.workintech.ecommerce.backend.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
 
     private AuthenticationService authenticationService;
